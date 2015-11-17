b0f72183f77f6d7c3a3455f76123089d319bf316
Ubuntu <ubuntu@ip-172-31-34-9.us-west-2.compute.internal>
2015-11-17 15:36:18 -0800

    Add magic method.

--- user/lib/Ocdla/UserDownload.php
+++ user/lib/Ocdla/UserDownload.php
@@ -7,17 +7,36 @@ class UserDownload
 {
 
 	public $info;
-
-	public function __construct($memberid,$productid)
-	{
-
-		if(empty($productid)) throw new Exception('Class UserDownload: constructor expects parameter $productid.');
 	
-		if(empty($memberid)) throw new Exception('Class UserDownload: constructor expects parameter $memberid.');
+	public $type;
+	
+	const ownerpw = "ocdla";
+	
+	private $username;
+	
+	private $password;
+	
+	private $passphrase;
+	
+	private $productId;
+	
+	private $UserID;
+
+	public function __construct($memberId,$productId)
+	{
+		
+		$this->productId = $productId;
+		
+		
+		$this->UserID = $memberId;
+
+		if(empty($productId)) throw new \Exception('Class UserDownload: constructor expects parameter $productid.');
+	
+		if(empty($memberId)) throw new \Exception('Class UserDownload: constructor expects parameter $memberid.');
 
 
 
-		$stmt = db_query('SELECT * FROM {members} m LEFT JOIN {downloads} d ON(m.id=d.memberid) JOIN {catalog} cat ON(cat.i=d.productid) WHERE memberid=:1 AND productid=:2',array($memberid,$productid));
+		$stmt = db_query('SELECT * FROM {members} m LEFT JOIN {downloads} d ON(m.id=d.memberid) JOIN {catalog} cat ON(cat.i=d.productid) WHERE memberid=:1 AND productid=:2',array($memberId,$productId));
 		$this->info = $stmt->fetch_assoc();
 		
 		clearstatcache();
@@ -37,8 +56,6 @@ class UserDownload
 		return $this->info['SoftwareType'];
 	}
 
-
-
 	public function getProductId()
 	{
 		return $this->info['i'];
@@ -48,6 +65,119 @@ class UserDownload
 	{
 		return $this->info['id'];
 	}
+
+	private function getPassword($security = "local")
+	{
+		// make sure the members table is also located locally
+		
+		
+		
+		// so the webserver should be serving up these files but instead they should be located on the pacinfo server in a protected directory
+	}
+
+	public function setFileCreationTime($time=null)
+	{
+		$update = new DBQuery(
+			array(
+				"type"=>"update",
+				"tables"=>array(
+					0 => array(
+						"name"=>"downloads",
+						"op" => "",
+						"fields" => array()
+					)
+				),//tables
+				"fields"=>array(
+					"file_creation_time"=>$time
+				),//fields
+				"where"=>array(
+					"memberid={$this->UserID}",
+					"AND productid={$this->productId}"
+				)
+			)
+		);
+		
+		$update->exec();
+	}
+
+	public function setNotificationTime($time = NULL)
+	{
+		$update = new DBQuery(
+			array(
+				"type"=>"update",
+				"tables"=>array(
+					0 => array(
+						"name"=>"downloads",
+						"op" => "",
+						"fields" => array()
+					)
+				),//tables
+				"fields"=>array(
+					"notification_time"=>$time
+				),//fields
+				"where"=>array(
+					"memberid={$this->UserID}",
+					"productid={$this->productId}"
+				)
+			)
+		);
+		
+		$update->exec();
+	}	
+	
+	public function createUserFile()
+	{
+		if( $this->type == "pdf" ) $this->createUserPdf();
+		else if( $this->type == "zip" ) $this->createUserZip();
+	}
+	
+	private function createFile()
+	{
+	/**
+	 * in the future, this function might check for things like an updated version of the file
+	 * for now, return FALSE if the file has already been created
+	 */
+			$user_filename = "{$this->username}_{$this->filename}";
+			if(file_exists( DOWNLOAD_PATH . "/{$this->username}_{$this->filename}") )
+			{
+				return FALSE;
+			}
+		else return TRUE;
+	}
+	
+	private function createUserPdf()
+	{	
+		if( $this->type != "pdf" ) throw new Exception('Class UserDownload: function createUserPdf being invoked on a non-pdf UserDownload instance!'); 
+		
+		//
+		// do some stuff to lookup the user and password, and perhaps what file they
+		// are supposed to download.
+		//
+	
+		// don't recreate the file if it already exists
+		// @jbernal 2012-01-05 recreate the user file regardless of whether it exists or not
+		$user_filename = "{$this->username}_{$this->filename}";
+		/*	if(  $this->createFile()===FALSE ) {
+		tail("Class UserDownloads: skipping file {$user_filename} (file already exists.)");
+			return FALSE;
+		} else if ($this->createFile()===TRUE ) {
+		*/
+		if (TRUE)
+		{
+			//if( file_exists( DOWNLOAD_PATH . "/pdftk.txt") ) unlink( DOWNLOAD_PATH . "/pdftk.txt" );
+
+			// WE NEED TO RECREATE THE FILE EVEN IF IT ALREADY EXISTS!  in order to remove passwords on existing jbernal_duii files!!
+
+			// we need to 			
+			//		$pdftk_command = "/usr/local/bin/pdftk ".UPLOAD_PATH . "/{$this->filename} output " . DOWNLOAD_PATH . "/{$this->username}_{$this->filename} user_pw {$this->passphrase} owner_pw ".self::ownerpw." allow Printing CopyContents verbose >> pdftk.txt";	
+		
+			$pdftk_command = "/usr/local/bin/pdftk ".UPLOAD_PATH . "/{$this->filename} output " . DOWNLOAD_PATH . "/{$this->username}_{$this->filename} owner_pw ".self::ownerpw." allow Printing CopyContents verbose >> pdftk.txt";	
+			chdir( DOWNLOAD_PATH );
+			exec( $pdftk_command, $output, $return_var );
+			return $return_var;	
+		}
+	}
+
 	public function __toString()
 	{
 		$str = "<table style='border:1px solid #666;'>
