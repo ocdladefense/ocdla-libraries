d7fc40a73dd63e4184b6173803bfca86e98022a4
Ubuntu <ubuntu@ip-172-31-34-9.us-west-2.compute.internal>
2015-11-18 11:38:02 -0800

    Consolidate pdf/zip creation into user downloads.

--- user/lib/Ocdla/UserDownload.php
+++ user/lib/Ocdla/UserDownload.php
@@ -5,39 +5,92 @@ namespace Ocdla;
 
 class UserDownload
 {
-
-	public $info;
+	/**
+	 * variable downloadPath
+	 *
+	 * The path prepended to saved files. The
+	 * complete filename should be saved as:
+	 * self::$downloadPath . '/'. $user_filename
+	 * where $user_filename = "{$this->username}_{$this->filename}".
+	 */
+	static $destPath = DOWNLOAD_PATH;
+	
+	static $sourcePath = UPLOAD_PATH;
 	
 	public $type;
 	
 	const ownerpw = "ocdla";
 	
+	private $userId;
+	
+	/**
+	 * var username
+	 *
+	 * The username of the user associated with this download.
+	 */
 	private $username;
 	
 	private $password;
 	
-	private $passphrase;
-	
 	private $productId;
 	
-	private $UserID;
-
-	public function __construct($memberId,$productId)
-	{
-		
-		$this->productId = $productId;
-		
-		
-		$this->UserID = $memberId;
-
-		if(empty($productId)) throw new \Exception('Class UserDownload: constructor expects parameter $productid.');
+	private $productName;
 	
-		if(empty($memberId)) throw new \Exception('Class UserDownload: constructor expects parameter $memberid.');
+	private $downloadLocation;
 
+	public function setDownloadPath($path)
+	{
+		self::$downloadPath = $path;
+	}
 
+	public static function newFromDownloadId($id)
+	{
+	
+	}
+	
+	public static function newFromParams($params)
+	{
+		if(empty($params['memberId'])||empty($params['productId']))
+		{
+			throw new \Exception('Class UserDownload: constructor expects parameter memberId.');
+		}
+		$stmt = db_query('SELECT d.i AS downloadId FROM {downloads} d WHERE memberid=:memberId AND productid=:productId',
+			array('memberId'=>$params['memberId']
+				,'productId'=>$params['productId']),'pdo');
+		$info = $stmt->fetch();
+		return new UserDownload($info['downloadId']);
+	}
 
-		$stmt = db_query('SELECT * FROM {members} m LEFT JOIN {downloads} d ON(m.id=d.memberid) JOIN {catalog} cat ON(cat.i=d.productid) WHERE memberid=:1 AND productid=:2',array($memberId,$productId));
-		$this->info = $stmt->fetch_assoc();
+	public function __construct($downloadId)
+	{	
+		if(empty($downloadId)) throw new \Exception('Class UserDownload: constructor expects parameter downloadId.');
+		
+		$stmt = db_query('SELECT m.username, m.autoId, m.id AS memberId, cat.i AS productId, cat.DownloadLocation, cat.Item, cat.SoftwareType, d.* FROM {members} m LEFT JOIN {downloads} d ON(m.id=d.memberid) JOIN {catalog} cat ON(cat.i=d.productid) WHERE d.i=:downloadId',
+			array('downloadId'=>$downloadId),'pdo');
+		
+		$info = $stmt->fetch();
+		if(!$info) throw new \Exception("No download with id, {$downloadId}, exists!");
+		
+		$this->productId 				= $info['productId'];
+		
+		$this->downloadId				= $info['i'];
+		
+		$this->userId 					= $info['memberId'];
+		
+		$this->productName			= substr($info["Item"],0,strpos($info["Item"],'-'));
+		
+		$this->type 						= $info['SoftwareType'];
+		
+		$path 									= explode('\\',$info["DownloadLocation"]);
+		$this->filename 				= array_pop($path);
+		
+		$this->username 				= $info['username'];
+		
+		$this->password 				= $info['password'];
+		
+		$this->userFilename 		= "{$this->username}_{$this->filename}";
+		$this->userFilename			= $this->type == 'zip'? $this->userFilename .'.zip':
+																	$this->userFilename;
 		
 		clearstatcache();
 		/* one last error should be thrown if we can't find the file associated with this product */
@@ -45,34 +98,40 @@ class UserDownload
 		/**
 		 * Check to make sure the file referred to actually exists
 		 */
-		if(false&&!file_exists($this->info["DownloadLocation"]))
+		if(false&&!file_exists($this->downloadLocation))
 		{
-			throw new Exception("Class UserDownload: Associated file {$this->info['DownloadLocation']} does not exist on this filesystem.");
+			throw new \Exception("Class UserDownload: Associated file {$this->downloadLocation} does not exist on this filesystem.");
 		}
 	}
-	
+
+	public function fileExists()
+	{
+		return file_exists(self::$sourcePath .'/' . $this->filename);
+	}
+
 	public function getDownloadType()
 	{
-		return $this->info['SoftwareType'];
+		return $this->type;
+	}
+
+	public function getProductName()
+	{
+		return $this->productName;
 	}
 
 	public function getProductId()
 	{
-		return $this->info['i'];
+		return $this->productId;
 	}
 	
 	public function getMemberId()
 	{
-		return $this->info['id'];
+		return $this->userId;
 	}
-
-	private function getPassword($security = "local")
+	
+	public function getUserId()
 	{
-		// make sure the members table is also located locally
-		
-		
-		
-		// so the webserver should be serving up these files but instead they should be located on the pacinfo server in a protected directory
+		return $this->userId;
 	}
 
 	public function setFileCreationTime($time=null)
@@ -91,7 +150,7 @@ class UserDownload
 					"file_creation_time"=>$time
 				),//fields
 				"where"=>array(
-					"memberid={$this->UserID}",
+					"memberid={$this->userId}",
 					"AND productid={$this->productId}"
 				)
 			)
@@ -116,7 +175,7 @@ class UserDownload
 					"notification_time"=>$time
 				),//fields
 				"where"=>array(
-					"memberid={$this->UserID}",
+					"memberid={$this->userId}",
 					"productid={$this->productId}"
 				)
 			)
@@ -131,68 +190,102 @@ class UserDownload
 		else if( $this->type == "zip" ) $this->createUserZip();
 	}
 	
-	private function createFile()
+	private function userFileExists($basePath)
 	{
-	/**
-	 * in the future, this function might check for things like an updated version of the file
-	 * for now, return FALSE if the file has already been created
-	 */
-			$user_filename = "{$this->username}_{$this->filename}";
-			if(file_exists( DOWNLOAD_PATH . "/{$this->username}_{$this->filename}") )
-			{
-				return FALSE;
-			}
-		else return TRUE;
+		$path = isset($basePath)?$base_path:self::$sourcePath;
+		return file_exists($path . "/{$this->userFilename}");
+	}
+	
+	private function getZipCommand()
+	{
+		$executable = "/usr/bin/zip";
+		$options = "rv9";
+		$inFile = ".";
+		$outFile = "../{$this->userFilename}";
+		return $executable . " -{$options} {$outFile} {$inFile}";
+		// $cmd = "\"\"\\inetpub\\ocdla\\html\\sites\\ocdla\\modules\\zip_download\\zip\" -rv9 \"..\\$filename"."_".$username."\" \".\"\"";
+	}
+	
+	private function createUserZip()
+	{
+		if($this->type!='zip') throw new \Exception("Cannot create a zip archive from a non-zip source!");
+		$sourceDir = self::$sourcePath . '/' . $this->filename;
+		if(chdir($sourceDir))
+		{
+			return shell_exec($this->getZipCommand());
+		}
+		else throw new \Exception("Could not initialize zip creation within directory, {$sourceDir}.");
 	}
 	
 	private function createUserPdf()
 	{	
-		if( $this->type != "pdf" ) throw new Exception('Class UserDownload: function createUserPdf being invoked on a non-pdf UserDownload instance!'); 
+		if ($this->type != "pdf") throw new \Exception('Class UserDownload: function createUserPdf being invoked on a non-pdf UserDownload instance!'); 
 		
-		//
-		// do some stuff to lookup the user and password, and perhaps what file they
-		// are supposed to download.
-		//
-	
-		// don't recreate the file if it already exists
-		// @jbernal 2012-01-05 recreate the user file regardless of whether it exists or not
-		$user_filename = "{$this->username}_{$this->filename}";
-		/*	if(  $this->createFile()===FALSE ) {
-		tail("Class UserDownloads: skipping file {$user_filename} (file already exists.)");
-			return FALSE;
-		} else if ($this->createFile()===TRUE ) {
-		*/
-		if (TRUE)
+		if (chdir(self::$downloadPath))
 		{
-			//if( file_exists( DOWNLOAD_PATH . "/pdftk.txt") ) unlink( DOWNLOAD_PATH . "/pdftk.txt" );
-
-			// WE NEED TO RECREATE THE FILE EVEN IF IT ALREADY EXISTS!  in order to remove passwords on existing jbernal_duii files!!
-
-			// we need to 			
-			//		$pdftk_command = "/usr/local/bin/pdftk ".UPLOAD_PATH . "/{$this->filename} output " . DOWNLOAD_PATH . "/{$this->username}_{$this->filename} user_pw {$this->passphrase} owner_pw ".self::ownerpw." allow Printing CopyContents verbose >> pdftk.txt";	
+			exec($pdftk_command, $output, $return_var);
+		}
+		else
+		{
+			throw new \Exception("Could not change directory to {self::$downloadPath}.");
+		}
 		
-			$pdftk_command = "/usr/local/bin/pdftk ".UPLOAD_PATH . "/{$this->filename} output " . DOWNLOAD_PATH . "/{$this->username}_{$this->filename} owner_pw ".self::ownerpw." allow Printing CopyContents verbose >> pdftk.txt";	
-			chdir( DOWNLOAD_PATH );
-			exec( $pdftk_command, $output, $return_var );
-			return $return_var;	
+		$pdftk_command = "/usr/bin/pdftk ".self::$uploadPath . "/{$this->filename} output " . self::$downloadPath . "/{$this->userFilename} owner_pw ".self::$ownerpw." allow Printing CopyContents verbose >> /var/log/pdftk-web.log";	
+		
+		exec($pdftk_command, $output, $return_var);
+		return $return_var;	
+	}
+
+	private function fileStatusIcon($boolean)
+	{
+		if($boolean)
+		{
+			return "<img style='width:18px;' src='/sites/default/files/icons/status-ok.png' alt='File exists.' />";
+		}
+		else
+		{
+			return "<img style='width:18px;' src='/sites/default/files/icons/chrome_red_lock_details.png' alt='File doesn't exist.' />";
 		}
 	}
 
 	public function __toString()
 	{
-		$str = "<table style='border:1px solid #666;'>
+		$sourceFileStatus = $this->fileStatusIcon($this->fileExists());
+		$userFileStatus = $this->fileStatusIcon($this->userFileExists());
+									
+		$str = "<table style='margin-bottom:8px; width:475px; border:1px solid #666;'>
 			<thead>
 				<tr>
-				<th>FileName</th>
-				<th>Type</th>
-				<th>MemberId</th>
+					<th>FileExists?</th>
+					<th>FileName</th>
+					<th>Type</th>
+					<th>DownloadId</th>
+					<th>UserId</th>
+					<th>ProductId</th>
 				</tr>
 			</thead>
 			<tbody>
 				<tr>
-					<td>foo</td>
-					<td>bar</td>
-					<td>baz</td>
+					<td style='width:25px;text-align:center;'>
+						{$sourceFileStatus}
+					</td>
+					<td style='width:225px;text-align:center;'>{$this->filename}</td>
+					<td style='text-align:center;'>{$this->type}</td>
+					<td style='text-align:center;'>{$this->downloadId}</td>
+					<td style='text-align:center;'>{$this->userId}</td>
+					<td style='text-align:center;'>{$this->productId}</td>
+				</tr>
+				<tr>
+					<td colspan='6' style='text-align:center;background-color:#666;color:#fff;'>
+						Below is the user information for this file:
+					</td>
+				<tr style='border-top:1px solid #666;'>
+					<td style='text-align:center;'>
+						{$userFileStatus}
+					</td>
+					<td colspan='5'>
+						{$this->getZipCommand()}
+					</td>
 				</tr>
 			</tbody>
 		</table>";
