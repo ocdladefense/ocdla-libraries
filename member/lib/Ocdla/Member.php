4881c59972f50cbe094e5557788a1d6a7c440757
Jose Bernal <jbernal.web.dev@gmail.com>
2015-12-03 18:15:21 -0800

    update Member class.

--- member/lib/Ocdla/Member.php
+++ member/lib/Ocdla/Member.php
@@ -1,18 +1,47 @@
 <?php
-
+/**
+ * This is file-level documentation - Members.php
+ *
+ * Here is a full description of what this file does
+ *
+ *
+ * 
+ */
+ 
+/**
+ * Represents an individual OCDLA member
+ *
+ * 
+ *
+ */
 namespace Ocdla;
 
 class Member
 {
 	private $data;
 	
+	/**
+	 * Constructor for Member
+	 *
+	 * Instantiate an OCDLA member with data from
+	 * the `members` table.
+	 *
+	 * @param int $memberId The `members.id` of the Member to load.
+	 *
+	 * @return void
+	 */
 	public function __construct($memberId)
 	{
 		if (!isset($memberId)) throw new \Exception('Invalid retrieval of member record in '.__FUNCTION__);	
 		
-		$stmt = db_query("SELECT m.*, ci.value AS email FROM {members} m LEFT JOIN {member_contact_info} ci ON(ci.contact_id=m.id) WHERE m.id=:1 AND type='email'",array($memberId),'mysql');
+		$stmt = db_query("SELECT m.*, ci.value AS email FROM {members} m LEFT JOIN {member_contact_info} ci ON(ci.contact_id=m.id) WHERE m.id=:memberId AND type='email'",array('memberId'=>$memberId),'pdo');
 		
-		$this->data = $stmt->fetch_assoc();
+		$this->data = $stmt->fetch();
+	}
+	
+	public static function newFromUsername($username)
+	{
+		
 	}
 
 	
